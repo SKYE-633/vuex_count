465d55e65dfbade3014b99eb41c348e7f32aac9d
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-22 16:23:09 +0800

    vuex还没弄完 等等再补

---
+++ src/components/Count.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <button @click="increase">+</button>
+    <span>{{count}}</span>
+    <button @click="decrease">-</button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'Count',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+     count: 0
+    }
+  },
+  methods:{
+    increase () {
+      this.count++
+    },
+    drease () {
+      this.count--
+    }
+  },
+  computed: mapState({
+    
+  })
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
